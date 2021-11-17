1fac1f47b0842b9d7103d1f01693b661d1fc5a5e
Nick Yozov <n_yozov@hotmail.com>
2021-11-17 00:40:17 +0000

    add capybara test for product_details page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    1.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "User can navigate from home page to product detail" do
+    visit root_path
+    find_link("Details").trigger("click")
+
+    expect(page).to have_css 'section.products-show'
+    
+  end
+end
